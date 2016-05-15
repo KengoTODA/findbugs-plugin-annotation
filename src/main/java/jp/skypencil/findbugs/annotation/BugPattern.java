97eac1cd31f7c06f4032e1c356309904af286384
Kengo TODA <skypencil@gmail.com>
2016-05-15 08:48:50 +0800

    improve javadoc

--- src/main/java/jp/skypencil/findbugs/annotation/BugPattern.java
+++ src/main/java/jp/skypencil/findbugs/annotation/BugPattern.java
@@ -7,13 +7,27 @@ import java.lang.annotation.Target;
 
 import com.google.common.base.Function;
 
+/**
+ * <p>An annotation to describe bug pattern reported by {@link Detector}.</p>
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target({ ElementType.TYPE })
 public @interface BugPattern {
+    /**
+     * <p>Type of this BugPattern. Generally formatted in UPPER_SNAKE_CASE.</p>
+     */
     String type();
 
+    /**
+     * <p>Abbreviation of type of this BugPattern.</p>
+     */
     String abbrev();
 
+    /**
+     * <p>Category of bug pattern. Basically this value is one of {@link PredefinedCategory} but custom category is also supported.</p>
+     * 
+     * @see PredefinedCategory
+     */
     String category();
 
     boolean experimental() default false;
--- src/main/java/jp/skypencil/findbugs/annotation/Detector.java
+++ src/main/java/jp/skypencil/findbugs/annotation/Detector.java
@@ -5,9 +5,24 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * <p>
+ * An annotation to annotate classes which provides Findbugs Detector. Generally
+ * they are implementation of {@link edu.umd.cs.findbugs.Detector} or
+ * {@link edu.umd.cs.findbugs.Detector2}.
+ * </p>
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target({ ElementType.TYPE })
 public @interface Detector {
+    /**
+     * <p>
+     * List of {@link BugPattern} reported by the annotated detector. Should not
+     * be empty.
+     * </p>
+     * 
+     * @see BugPattern
+     */
     BugPattern[] value();
 
     Speed speed() default Speed.DEFAULT;
